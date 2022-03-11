6ecd69c8bc86ac62b9d641096aaf3ff471ec2994
Lina-Humphris <postcrossing2002@mail.ru>
2022-03-11 17:31:55 +0300

    Add constructor and operator >> in class transaction

--- Bank/Bank/Transaction.cpp
+++ Bank/Bank/Transaction.cpp
@@ -1,1 +1,31 @@
 #include "Transaction.h"
+
+Transaction::Transaction() {
+	set_debit_account("");
+	set_transfer_account("");
+	set_amount(0);
+	set_currency_of_operation(0);
+	Date date_temp;
+	set_date(date_temp); //пока нинаю, нужно ли этот метод тут прописывать
+	set_status(0);
+}
+// Change
+std::istream& operator>>(istream& in, Transaction& t) {
+	string s;
+	cout << "Введите номер вашего счета: ";
+	cin >> s;
+	t.set_debit_account(s);
+	cout << "Введите счет, на который будет совершен перевод: ";
+	cin >> s;
+	t.set_transfer_account(s);
+	double temp_amount;
+	cout << "Введите сумму перевода: ";
+	cin >> temp_amount;
+	t.set_amount(temp_amount);
+	// добавить сравнение валют, и в случае совпадения set_currency_of_operation
+	//добавить проверку на лимит и на кол-во денег
+	Date date_temp;
+	t.set_date(date_temp);
+	//если все ок, ставим статус
+	return in;
+}
--- Bank/Bank/Transaction.h
+++ Bank/Bank/Transaction.h
@@ -14,13 +14,15 @@ private:
 	Date _date;
 	int _status;
 public:
-	Transaction(string debit_account, string transfer_account, double amount, int currancy_of_operation, Date date, int status);
+	Transaction();
 
-	void set_debit_account(string debit_account);
-	void set_transfer_account(string transfer_account);
-	void set_amount(double amount);
-	void set_currency_of_operation(int currency_of_operation);
-	void set_date();
-	void set_status(int status);
+	void set_debit_account(string debit_account) { _debit_account = debit_account; };
+	void set_transfer_account(string transfer_account) { _transfer_account = transfer_account; };
+	void set_amount(double amount) { _amount = amount; };
+	void set_currency_of_operation(int currency_of_operation) { _currency_of_operation = currency_of_operation; };
+	void set_date(Date date) { _date = date; };
+	void set_status(int status) { _status = status; };
+
+	friend std::istream& operator>>(istream& in, Transaction& t);
 };
 
