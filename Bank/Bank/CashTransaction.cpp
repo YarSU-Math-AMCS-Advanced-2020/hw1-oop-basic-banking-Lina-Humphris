36dbc3189958852119231acbb8a6ce29fb8f8dd4
Lina-Humphris <postcrossing2002@mail.ru>
2022-03-13 13:14:58 +0300

    Forgot cpp last time

--- Bank/Bank/CashTransaction.cpp
+++ Bank/Bank/CashTransaction.cpp
@@ -1,1 +1,10 @@
 #include "CashTransaction.h"
+
+CashTransaction::CashTransaction() {
+	set_debit_account("");
+	set_amount(0);
+	set_date();
+	set_status(false);
+	set_place(0);
+	set_place_number(0);
+}
