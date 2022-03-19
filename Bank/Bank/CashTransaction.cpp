00dcef440c365f9f94984ba5755ba9807f1106b5
Lina-Humphris <postcrossing2002@mail.ru>
2022-03-19 15:49:38 +0300

    Change debit to card

--- Bank/Bank/CashTransaction.cpp
+++ Bank/Bank/CashTransaction.cpp
@@ -1,7 +1,14 @@
 #include "CashTransaction.h"
 
+
+void CashTransaction::set_date() {
+	Date temp_date;
+	_date = temp_date;
+}
+
+
 CashTransaction::CashTransaction() {
-	set_debit_account("");
+	set_card_account("");
 	set_amount(0);
 	set_date();
 	set_status(false);
--- Bank/Bank/CashTransaction.h
+++ Bank/Bank/CashTransaction.h
@@ -7,7 +7,7 @@ using namespace std;
 class CashTransaction
 {
 protected:
-	string _debit_account;
+	string _card_account;
 	double _amount;
 	Date _date;
 	bool _status;
@@ -17,12 +17,11 @@ protected:
 public:
 	CashTransaction();
 
-	void set_debit_account(string debit_account) { _debit_account = debit_account; };
+	void set_card_account(string card_account) { _card_account = card_account; };
 	void set_amount(double amount) { _amount = amount; };
-	void set_date() {};
+	void set_date();
 	void set_status(bool status) { _status = status; };
 	void set_place(int place) { _place = place; };
 	void set_place_number(int place_number) { _place_number = place_number; };
 
 };
-
