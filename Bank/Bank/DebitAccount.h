a0b003fb36002392f56dcbc3dc3767877e9cc6d1
Lina-Humphris <postcrossing2002@mail.ru>
2022-03-19 15:00:54 +0300

    Add new property in Debit account

--- Bank/Bank/DebitAccount.h
+++ Bank/Bank/DebitAccount.h
@@ -12,6 +12,7 @@ private:
 	double _balance;
 	double _limit;
 	int _currency;
+	bool _has_card;
 
 public:
 	DebitAccount();
@@ -21,11 +22,13 @@ public:
 	void set_balance(double balance) { _balance = balance; };
 	void set_limit(double limit) { _limit = limit; };
 	void set_currency(int currency) { _currency = currency; };
+	void set_has_card(bool has_card) { _has_card = has_card; };
 
 	double get_limit() { return _limit; };
 	string get_debit_id() { return _debit_id; }
 	double get_balance() { return _balance; };
 	int get_currency() { return _currency; };
+	bool get_has_card() { return _has_card; }
 
 	friend std::istream& operator>>(istream& in, DebitAccount& t);
 
