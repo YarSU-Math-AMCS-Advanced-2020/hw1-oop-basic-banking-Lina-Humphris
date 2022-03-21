7c33054777932c7e072d40cd380ef88acc96368b
Lina-Humphris <postcrossing2002@mail.ru>
2022-03-21 12:02:11 +0300

    Add transaction for cards

--- Bank/Bank/Transaction.cpp
+++ Bank/Bank/Transaction.cpp
@@ -7,58 +7,105 @@ Transaction::Transaction() {
 	set_amount(0);
 	set_currency_of_operation(0);
 	Date date_temp;
-	set_date(date_temp); 
+	set_date(date_temp);
 	set_status(0);
 }
 
 std::istream& operator>>(istream& in, Transaction& t) {
 	Date date_temp;
+	t.set_date(date_temp);
+	bool is_accept = false;
 	int debit_currency = -1;
 	int transfer_currency = -1;
-	t.set_date(date_temp);
+	cout << "Выберите 0 если хотите проделать операцию с вкладами, 1 если с картами";
+	int n;
+	cin >> n;
 	string s;
-	cout << "Введите номер вашего счета: ";
-	cin >> s;
 	DataBase* data_base = DataBase::getInstance();
 	vector <DebitAccount> base_debit = data_base->get_base_debit();
-	bool is_accept = false;
-
-	while (!is_accept) {
-		for (int i = 0; i < base_debit.size(); i++) {
-			DebitAccount curr = base_debit[i];
-			if (curr.get_debit_id() == s) {
-				is_accept = true;
-				debit_currency = curr.get_currency();
+	vector <DebitCard> base_card = data_base->get_base_card();
+	if (n == 1) {
+		cout << "Введите номер вашей карты:";
+		cin >> s;
+		string debit;
+		while (!is_accept) {
+			for (int i = 0; i < base_card.size(); i++) {
+				DebitCard curr = base_card[i];
+				if (curr.get_card_id() == s) {
+					is_accept = true;
+					debit = curr.get_debit_id();
+					debit_currency = curr.get_debit_currency();
+				}
 			}
-		}
-		if (!is_accept) {
-			cout << "Счета с таким номером не существует, попробуйте еще раз: ";
-		}
-		if (!is_accept) cin >> s;
-	}
-	t.set_debit_account(s);
-	cout << "Введите счет, на который будет совершен перевод: ";
-	cin >> s;
-	is_accept = false;
-
-	while (!is_accept) {
-		for (int i = 0; i < base_debit.size(); i++) {
-			DebitAccount curr = base_debit[i];
-			if (curr.get_debit_id() == s) {
-				is_accept = true;
-				transfer_currency = curr.get_currency();
+			if (!is_accept) {
+				cout << "Карты с таким номером не существует, попробуйте еще раз: ";
 			}
+			if (!is_accept) cin >> s;
 		}
-		if (!is_accept) {
-			cout << "Счета с таким номером не существует, попробуйте еще раз: ";
+		t.set_debit_account(debit);
+		cout << "Введите номер карты, на который будет совершен перевод: ";
+		cin >> s;
+		is_accept = false;
+		while (!is_accept) {
+			for (int i = 0; i < base_card.size(); i++) {
+				DebitCard curr = base_card[i];
+				if (curr.get_debit_id() == s) {
+					is_accept = true;
+					debit = curr.get_debit_id();
+					transfer_currency = curr.get_debit_currency();
+				}
+			}
+			if (!is_accept) {
+				cout << "Карты с таким номером не существует, попробуйте еще раз: ";
+			}
+			if (debit == t.get_debit_account()) {
+				cout << "Номер введенной карты совпадает с номером карты с которой будет производиться перевод, попробуйте еще раз: ";
+				is_accept = false;
+			}
+			if (!is_accept) cin >> s;
 		}
-		if (s == t.get_debit_account()) {
-			cout << "Номер введенного счета совпадает с номером счета с которого будет производиться перевод, попробуйте еще раз: ";
-			is_accept = false;
-		}
-		if (!is_accept) cin >> s;
+		t.set_transfer_account(debit);
 	}
-	t.set_transfer_account(s);
+	else {
+		cout << "Введите номер вашего счета: ";
+		cin >> s;
+		while (!is_accept) {
+			for (int i = 0; i < base_debit.size(); i++) {
+				DebitAccount curr = base_debit[i];
+				if (curr.get_debit_id() == s) {
+					is_accept = true;
+					debit_currency = curr.get_currency();
+				}
+			}
+			if (!is_accept) {
+				cout << "Счета с таким номером не существует, попробуйте еще раз: ";
+			}
+			if (!is_accept) cin >> s;
+		}
+		t.set_debit_account(s);
+		cout << "Введите номер счета, на который будет совершен перевод: ";
+		cin >> s;
+		is_accept = false;
+
+		while (!is_accept) {
+			for (int i = 0; i < base_debit.size(); i++) {
+				DebitAccount curr = base_debit[i];
+				if (curr.get_debit_id() == s) {
+					is_accept = true;
+					transfer_currency = curr.get_currency();
+				}
+			}
+			if (!is_accept) {
+				cout << "Счета с таким номером не существует, попробуйте еще раз: ";
+			}
+			if (s == t.get_debit_account()) {
+				cout << "Номер введенного счета совпадает с номером счета с которого будет производиться перевод, попробуйте еще раз: ";
+				is_accept = false;
+			}
+			if (!is_accept) cin >> s;
+		}
+		t.set_transfer_account(s);
+	}
 	double temp_amount;
 	cout << "Введите сумму перевода: ";
 	cin >> temp_amount;
@@ -97,6 +144,7 @@ std::istream& operator>>(istream& in, Transaction& t) {
 				curr.set_balance(curr.get_balance() + temp_amount);
 			}
 		}
+		data_base->set_base_debit(base_debit);
 	}
 	t.set_amount(temp_amount);
 	if (is_accept) t.set_status(1);
